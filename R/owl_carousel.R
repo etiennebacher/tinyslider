1d1065fccb7f9bcd7a1dcc0ab858236154500351
etiennebacher <etienne.bacher@orange.fr>
2020-11-03 19:37:25 +0100

    remove some useless exports

--- R/owl_carousel.R
+++ R/owl_carousel.R
@@ -23,7 +23,7 @@
 #'
 #' @import htmlwidgets
 #'
-#' @export
+#' @export owl_carousel
 owl_carousel <- function(id, ..., options = list()) {
 
   tag <- htmltools::tags$div(
@@ -59,59 +59,4 @@ owl_carousel <- function(id, ..., options = list()) {
     x,
     package = 'shinymisc'
   )
-}
-
-
-# owl_carousel <- function(message, width = NULL, height = NULL, elementId = NULL) {
-#
-#   x = list(
-#     message = message
-#   )
-#
-#   htmlwidgets::createWidget(
-#     name = 'owl_carousel',
-#     x,
-#     width = width,
-#     height = height,
-#     package = 'shinymisc',
-#     elementId = elementId
-#   )
-# }
-
-
-
-
-
-
-
-
-
-
-
-#' Shiny bindings for owl_carousel
-#'
-#' Output and render functions for using owl_carousel within Shiny
-#' applications and interactive Rmd documents.
-#'
-#' @param outputId output variable to read from
-#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
-#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
-#'   string and have \code{'px'} appended.
-#' @param expr An expression that generates a owl_carousel
-#' @param env The environment in which to evaluate \code{expr}.
-#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
-#'   is useful if you want to save an expression in a variable.
-#'
-#' @name owl_carousel-shiny
-#'
-#' @export
-owl_carouselOutput <- function(outputId, width = '100%', height = '400px'){
-  htmlwidgets::shinyWidgetOutput(outputId, 'owl_carousel', width, height, package = 'shinymisc')
-}
-
-#' @rdname owl_carousel-shiny
-#' @export
-renderOwl_carousel <- function(expr, env = parent.frame(), quoted = FALSE) {
-  if (!quoted) { expr <- substitute(expr) } # force quoted
-  htmlwidgets::shinyRenderWidget(expr, owl_carouselOutput, env, quoted = TRUE)
 }
