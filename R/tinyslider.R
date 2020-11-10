ecdb7a0a6525a80e72bc9a85833ce9505662e826
etiennebacher <etienne.bacher@orange.fr>
2020-11-10 21:22:43 +0100

    Add functions for shiny output, closes #7

--- R/tinyslider.R
+++ R/tinyslider.R
@@ -103,3 +103,31 @@ tinyslider <- function(id, ..., options = list()) {
     )
 }
 
+
+#' Shiny bindings for tinyslider
+#'
+#' Output and render functions for using tinyslider within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a tinyslider
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name tinyslider-shiny
+#'
+#' @export
+tinysliderOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'tinyslider', width, height, package = 'shinymisc')
+}
+
+#' @rdname tinyslider-shiny
+#' @export
+renderTinyslider <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, tinysliderOutput, env, quoted = TRUE)
+}
