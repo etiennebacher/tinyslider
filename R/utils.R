f22a5f02dadfddc8bf0ce7323b932b542c41bf2c
etiennebacher <etienne.bacher@orange.fr>
2020-11-20 19:34:28 +0100

    add error message in plot_to_card

--- R/utils.R
+++ R/utils.R
@@ -1,17 +1,19 @@
 #' Convert a plot to a card
 #'
-#' @param gg the name of a plot (works only for ggplots for now)
+#' @param plot the name of a plot (works only for ggplots for now)
 #'
 #' @export
-#'
-#' @examples
-plot_to_card <- function(gg){
+plot_to_card <- function(plot){
+
+  if (!("gg" %in% class(plot)))
+    stop("This function only works with ggplots")
+
   temp <- tempfile(fileext = ".png")
 
   on.exit({
     file.remove(temp)
   })
 
-  ggplot2::ggsave(filename = temp, plot = gg)
+  ggplot2::ggsave(filename = temp, plot = plot)
   paste0("data:image/png;base64,", base64enc::base64encode(temp))
 }
